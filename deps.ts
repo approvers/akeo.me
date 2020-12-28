3ba6b59ad8ec028c9b0eef9bf5c4886e716aca42
MikuroXina <ryosukadnak@gmail.com>
2020-12-28 14:45:22 +0900

    feat: Setup

---
+++ deps.ts
@@ -0,0 +1,15 @@
+export {
+  Fragment,
+  h,
+} from "https://deno.land/x/dext@0.10.1/deps/preact/mod.ts";
+export {
+  useEffect,
+  useState,
+} from "https://deno.land/x/dext@0.10.1/deps/preact/hooks.ts";
+export type {
+  AppProps,
+  GetStaticData,
+  GetStaticDataContext,
+  GetStaticPaths,
+  PageProps,
+} from "https://deno.land/x/dext@0.10.1/mod.ts";
